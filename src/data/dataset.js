9c286c0dd5d1b2ef1fac717674210b0c0e99f1da
JaviFerradaDev <javieraferrada68@gmail.com>
2024-03-26 13:35:00 -0300

    Actualización dataset hasta Scala

--- src/data/dataset.js
+++ src/data/dataset.js
@@ -190,5 +190,193 @@ export default [
   "extraInfo": {
     "imageSource": "https://drive.google.com/file/d/1Wkd9oXtg2piQFLXk4XNPWZpriWYCg8Zc/view?usp=drive_link"
   }
+},
+{
+  "id": "radia-perlman",
+  "name": "Radia Perlman",
+  "shortDescription": "Ingeniera de redes, inventora del algoritmo Spanning Tree.",
+  "description": "Científica de la computación estadounidense, ha dejado una marca indeleble en el mundo de las redes informáticas y la seguridad. Conocida como \"la madre de Internet\", su invención del algoritmo Spanning Tree Protocol (STP) revolucionó la conectividad de redes y sentó las bases para la infraestructura de Internet moderna. A pesar de operar en un campo dominado por hombres, Perlman ha demostrado que las mujeres pueden liderar avances tecnológicos vitales. Su compromiso con la resiliencia de las redes y la ciberseguridad ha influido en la estabilidad y confiabilidad de la comunicación en línea.",
+  "imageUrl": "https://laboratoria-dataverse-talks.netlify.app/public/radia-perlman.jpg",
+  "facts": {
+    "yearOfBirth": "1951",
+    "birthPlace": "Portsmouth, Estados Unidos",
+    "mainField": "Ciencia de la Computación"
+  },
+  "extraInfo": {
+    "imageSource": "https://hackaday.com/2018/05/29/spanning-the-tree-dr-radia-perlman-untangling-networks/"
+  }
+},
+{
+  "id": "shafi-goldwasser",
+  "name": "Shafi Goldwasser",
+  "shortDescription": "Científica de la computación, ganadora del Premio Turing.",
+  "description": "Es una destacada criptógrafa y científica de la computación que ha contribuido significativamente a la teoría de la computación y la seguridad de la información. Su trabajo en criptografía de probabilidad y complejidad computacional ha sentado las bases para la seguridad en línea y la privacidad de datos en un mundo cada vez más digitalizado. Goldwasser es un ejemplo de cómo la ciencia de la computación puede abordar desafíos contemporáneos y proteger la información en la era de la información.",
+  "imageUrl": "https://laboratoria-dataverse-talks.netlify.app/public/shafi-goldwasser.jpg",
+  "facts": {
+    "yearOfBirth": "1958",
+    "birthPlace": "Nueva York, Estados Unidos",
+    "mainField": "Ciencia de la Computación"
+  },
+  "extraInfo": {
+    "imageSource": "https://www.jahresbericht2019.unibe.ch/successes_2019/einstein_lectures/index_eng.html"
+  }
+},
+{
+  "id": "chien-shiung-wu",
+  "name": "Chien-Shiung Wu",
+  "shortDescription": "Física experimental, contribuyó a la física nuclear.",
+  "description": "Física experimental de origen chino, desafiando los prejuicios de género y nacionalidad, realizó experimentos fundamentales en física de partículas y contribuyó significativamente a la comprensión de la desintegración beta. Su famoso experimento \"Wu\" refutó la paridad en la física de partículas, lo que cambió la forma en que los científicos comprenden las fuerzas fundamentales de la naturaleza. Wu demostró que el talento y la perseverancia pueden superar cualquier barrera, y su legado sigue siendo un faro de inspiración para científicas en todo el mundo.",
+  "imageUrl": "https://laboratoria-dataverse-talks.netlify.app/public/chien-shiung-wu.jpg",
+  "facts": {
+    "yearOfBirth": "1912",
+    "yearOfDeath": "1997",
+    "birthPlace": "Liuhe, Taicang, China",
+    "mainField": "Física"
+  },
+  "extraInfo": {
+    "imageSource": "https://www.thoughtco.com/chien-shiung-wu-biography-3530366"
+  }
+},
+{
+  "id": "hedy-lamarr",
+  "name": "Hedy Lamarr",
+  "shortDescription": "Actriz e inventora, contribuyó al desarrollo de la tecnología de espectro ensanchado.",
+  "description": "Actriz austroamericana y científica, fue una mente brillante en la intersección de la tecnología y el entretenimiento. Junto con el compositor George Antheil, inventó un sistema de comunicación secreta durante la Segunda Guerra Mundial que sentó las bases para la tecnología de espectro ensanchado y la comunicación inalámbrica moderna. Lamarr demostró que las mujeres pueden destacar tanto en el cine como en la ciencia, y su legado como inventora ha sido fundamental para el desarrollo de la tecnología de comunicación inalámbrica que usamos en la actualidad.",
+  "imageUrl": "https://laboratoria-dataverse-talks.netlify.app/public/hedy-lamarr.jpg",
+  "facts": {
+    "yearOfBirth": "1914",
+    "yearOfDeath": "2000",
+    "birthPlace": "Viena, Austria",
+    "mainField": "Actuación, Física"
+  },
+  "extraInfo": {
+    "imageSource": "https://www.sparkfun.com/news/6147"
+  }
+},
+{
+  "id": "maria-goeppert-mayer",
+  "name": "Maria Goeppert Mayer",
+  "shortDescription": "Física teórica, ganadora del Premio Nobel de Física.",
+  "description": "Física teórica alemana-estadounidense, dejó una profunda huella en la física nuclear y la mecánica cuántica. Fue la segunda mujer en recibir el Premio Nobel de Física y su trabajo pionero en la estructura de capas nucleares revolucionó nuestra comprensión de los núcleos atómicos. A pesar de las barreras de género en la academia, su dedicación y talento la llevaron a contribuir significativamente a la teoría nuclear, abriendo el camino para futuras investigadoras en un campo dominado por hombres. Maria Goeppert Mayer es un ejemplo inspirador de excelencia científica y perseverancia en la búsqueda del conocimiento.",
+  "imageUrl": "https://laboratoria-dataverse-talks.netlify.app/public/maria-goeppert-mayer.jpg",
+  "facts": {
+    "yearOfBirth": "1906",
+    "yearOfDeath": "1972",
+    "birthPlace": "Kattowitz, Alemania",
+    "mainField": "Física"
+  },
+  "extraInfo": {
+    "imageSource": "https://www.wsemexhibit.org/maria-goeppert-mayer.html"
+  }
+},
+{
+  "id": "lise-meitner",
+  "name": "Lise Meitner",
+  "shortDescription": "Física nuclear, contribuyó a la teoría de la fisión nuclear.",
+  "description": "Física austriaca-sueca, dejó un legado imborrable en la física nuclear y la radioquímica. A pesar de enfrentar obstáculos de género en una era dominada por hombres, colaboró con Otto Hahn en el descubrimiento de la fisión nuclear, un avance revolucionario. Aunque no compartió el Premio Nobel por este logro, su contribución fue crucial para comprender la energía nuclear y la física de partículas. Su determinación y pasión por la ciencia han inspirado a generaciones de científicas. Lise Meitner es un símbolo perdurable de la perseverancia femenina y el impacto duradero que las mujeres pueden tener en la ciencia.",
+  "imageUrl": "https://laboratoria-dataverse-talks.netlify.app/public/lise-meitner.jpg",
+  "facts": {
+    "yearOfBirth": "1878",
+    "yearOfDeath": "1968",
+    "birthPlace": "Viena, Austria",
+    "mainField": "Física"
+  },
+  "extraInfo": {
+    "imageSource": "https://www.bing.com/images/create/lise-meitner-in-nuclear-physics-lab-in-19502c-retro/651196e6bbcb4009950e9955c2199760"
+  }
+},
+{
+  "id": "sau-lan-wu",
+  "name": "Sau Lan Wu",
+  "shortDescription": "Física de partículas, contribuyó a experimentos clave en física de alta energía.",
+  "description": "Física experimental sinoamericana, ha sido una fuerza motriz en la física de partículas. Destacó en experimentos en el CERN y lideró investigaciones cruciales, como el descubrimiento del quark c, que expandieron nuestro entendimiento de la estructura fundamental de la materia. Wu superó desafíos en una disciplina dominada por hombres y ha sido un faro de inspiración para científicas de todo el mundo. Sau Lan Wu es un ejemplo de excelencia científica y de cómo las mujeres pueden dejar huella en la investigación de vanguardia.",
+  "imageUrl": "https://laboratoria-dataverse-talks.netlify.app/public/sau-lan-wu.jpg",
+  "facts": {
+    "yearOfBirth": "1940",
+    "birthPlace": "Hong Kong, China",
+    "mainField": "Física de Partículas"
+  },
+  "extraInfo": {
+    "imageSource": "https://repository.aip.org/islandora/object/nbla%3A315195"
+  }
+},
+{
+  "id": "jocelyn-bell-burnell",
+  "name": "Jocelyn Bell Burnell",
+  "shortDescription": "Astrónoma y astrofísica, co-descubridora de los púlsares.",
+  "description": "Astrofísica británica, es una figura extraordinaria en la ciencia. Durante su doctorado, hizo un descubrimiento revolucionario al detectar los primeros púlsares, estrellas de neutrones altamente magnetizadas que emiten pulsos regulares de radiación. A pesar de que su asesor recibió el Premio Nobel por este descubrimiento, Bell Burnell se ha convertido en un símbolo de perseverancia y contribución de las mujeres a la astronomía. Su carrera distinguida y su activismo en favor de la diversidad de género en la ciencia han inspirado a generaciones futuras de científicas. Jocelyn Bell Burnell es un ejemplo de valentía y excelencia científica.",
+  "imageUrl": "https://laboratoria-dataverse-talks.netlify.app/public/jocelyn-bell-burnell.jpg",
+  "facts": {
+    "yearOfBirth": "1943",
+    "birthPlace": "Belfast, Reino Unido",
+    "mainField": "Astronomía, Astrofísica"
+  },
+  "extraInfo": {
+    "imageSource": "https://womeninscienceweadmire.icfo.eu/wiswa-2/jocelyn-bell/"
+  }
+},
+{
+  "id": "dorothy-crowfoot-hodgkin",
+  "name": "Dorothy Crowfoot Hodgkin",
+  "shortDescription": "Química, ganadora del Premio Nobel de Química.",
+  "description": "Química británica, es una figura icónica en la ciencia que superó las barreras de género de su época. Su trabajo revolucionario en la cristalografía de rayos X permitió determinar la estructura de importantes biomoléculas, como la penicilina, la vitamina B12 y la insulina. Fue galardonada con el Premio Nobel de Química en 1964, siendo la tercera mujer en recibir este prestigioso reconocimiento. Hodgkin no solo fue una científica excepcional, sino también una defensora de la igualdad de género en la ciencia, allanando el camino para futuras generaciones de científicas y dejando un legado duradero en la investigación y la equidad.",
+  "imageUrl": "https://laboratoria-dataverse-talks.netlify.app/public/dorothy-crowfoot-hodgkin.jpg",
+  "facts": {
+    "yearOfBirth": "1910",
+    "yearOfDeath": "1994",
+    "birthPlace": "El Cairo, Egipto / Shipston-on-Stour, Reino Unido",
+    "mainField": "Química"
+  },
+  "extraInfo": {
+    "imageSource": "https://www.some.ox.ac.uk/news/jan-royall-why-we-must-champion-dorothy-hodgkin-for-the-50-note/"
+  }
+},
+{
+  "id": "marie-curie",
+  "name": "Marie Curie",
+  "shortDescription": "Física y química, ganadora de dos Premios Nobel.",
+  "description": "física y química de origen polaco y nacionalizada francesa, es una de las figuras más icónicas en la historia de la ciencia. Ganadora de dos premios Nobel (Física en 1903 y Química en 1911), Curie realizó investigaciones fundamentales sobre la radiactividad y el descubrimiento de los elementos radio y polonio. Su dedicación y valentía en la investigación científica, a pesar de los desafíos de la discriminación de género y los riesgos de la radiación, la convierten en un modelo a seguir para científicas de todo el mundo.",
+  "imageUrl": "https://laboratoria-dataverse-talks.netlify.app/public/marie-curie.jpg",
+  "facts": {
+    "yearOfBirth": "1867",
+    "yearOfDeath": "1934",
+    "birthPlace": "Varsovia, Polonia",
+    "mainField": "Física y Química"
+  },
+  "extraInfo": {
+    "imageSource": "https://www.historyextra.com/period/first-world-war/life-of-the-week-marie-curie/"
+  }
+},
+{
+  "id": "marjory-stephenson",
+  "name": "Marjory Stephenson",
+  "shortDescription": "Microbióloga y bioquímica, investigó en enzimología.",
+  "description": "Bioquímica británica, brilló como científica en una época en la que las mujeres enfrentaban obstáculos muy significativos en la investigación científica. Su trabajo pionero en microbiología, enzimología y bioquímica contribuyó al entendimiento de procesos fundamentales en la vida. Fue una de las primeras mujeres en ser elegida miembro de la Royal Society, un reconocimiento a su destacada contribución a la ciencia. Además, fue una mentora y defensora de las mujeres en la ciencia, inspirando y allanando el camino para futuras generaciones.",
+  "imageUrl": "https://laboratoria-dataverse-talks.netlify.app/public/marjory-stephenson.jpg",
+  "facts": {
+    "yearOfBirth": "1885",
+    "yearOfDeath": "1948",
+    "birthPlace": "Londres, Reino Unido",
+    "mainField": "Química, Microbiología, Bioquímica"
+  },
+  "extraInfo": {
+    "imageSource": "https://microbiologysociety.org/grants-prizes/all-prizes-and-competitions-/prize-lectures/marjory-stephenson-prize-lecture/marjory-stephenson-a-short-history.html"
+  }
+},
+{
+  "id": "Assembler",
+  "name": "Assembler",
+  "shortDescription": "Assembler es un lenguaje de programación de bajo nivel que proporciona una representación simbólica de las instrucciones de máquina.",
+  "description": "Assembler es un lenguaje de programación de bajo nivel utilizado para escribir programas directamente en el lenguaje de máquina de un ordenador. Aunque es complejo y propenso a errores, Assembler ofrece un control preciso sobre el hardware y es necesario para tareas de programación de sistemas y desarrollo de controladores. Su sintaxis específica de la arquitectura y su naturaleza cercana al hardware lo hacen adecuado para programadores con conocimientos avanzados en informática y sistemas embebidos.",
+  "imageUrl": "https://laboratoria-dataverse-talks.netlify.app/public/rosalind-franklin.jpg",
+  "imagepath" : "C:/Users/USUARIO/Documents/GitHub/Data-Verse/src/images/24A Assembler.jpg",
+  "facts": {
+    "yearOfBirth": "1920",
+    "creator": "",
+    "companiesUsingit": ["Intel","IBM"]
+  },
+  "extraInfo": {
+    "imageSource": ""
+  }
 }
 ]
